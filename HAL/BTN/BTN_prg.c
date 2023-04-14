5d4905615eb58ca364fab582fcd6b4402b12a5e4
TaqiEldeen <takeycoo@gmail.com>
2023-04-14 17:06:34 +0200

    Add BTN module (The emergency button)

---
+++ HAL/BTN/BTN_prg.c
@@ -0,0 +1,45 @@
+/*********************************************************/
+/***********		Author: TaqiEldeen	 	**************/
+/***********		Layer: 				 	**************/
+/***********		Component: 				**************/
+/***********		Version: 1.00		 	**************/
+/***********		Date: 14 Apr 2023		**************/
+/*********************************************************/
+
+/* LIB inclusion */
+#include "../../LIB/BIT_MATH.h"
+#include "../../LIB/STD_TYPES.h"
+
+/* MCAL inclusion*/
+#include "../../MCAL/EXTI/EXTI_int.h"
+
+/* System states inclusion */
+#include "../../App/states.h"
+
+/* Module inclusion */
+#include "BTN_pri.h"
+#include "BTN_int.h"
+
+extern u8 G_u8currentState;	/* The main state */
+
+/**********************************************************************************************************
+ * Description : Interface Function to initialize the battery
+ * Outputs     : void
+ * Inputs      : void
+ ***********************************************************************************************************/
+void BTN_vInit(){
+	/* Initialize external interrupt 0 on PD2 */
+    EXTI_vInitINT0();
+
+    EXTI_vReg_Func(BTN_vEmrgHandler, INT0_ID);
+}
+
+/**********************************************************************************************************
+ * Description : Private Function Handle the emergency button
+ * Outputs     : void
+ * Inputs      : void
+ ***********************************************************************************************************/
+static void BTN_vEmrgHandler() {
+	/* Change system state to emergency */
+	G_u8currentState = EMRG_STATE;
+}
